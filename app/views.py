f01a73f31ab5484058ecd42492a6494be317a24f
gomjellie <gomjellie@naver.com>
2018-11-18 03:49:58 +0900

    localhost -> colaboapp.cc

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, jsonify, request
 from app import app
 
-source_url_prefix = "http://localhost:4000/api/mobile/file?fileDirectory="
+source_url_prefix = "https://colaboapp.cc/api/mobile/file?fileDirectory="
 
 
 @app.route("/video", methods=["GET"])
